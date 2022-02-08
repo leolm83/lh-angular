308a84beff3402358d801ed7ec6feb29e06f9310
Leonardo Marques <leonardo.mateus01@hotmail.com>
2022-02-07 21:54:01 -0300

    finalizando projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
+import { RouterModule } from '@angular/router';
+import { PainelVagasComponent } from './painel-vagas/painel-vagas.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuralVagasComponent,
+    PainelVagasComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
